9f7098bc2e5ceb75487dd6582baa5665646b11e7
Cory Powell <coryrpowell@gmail.com>
2023-07-19 12:08:45 -0400

    Add drive method and tests

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,11 +1,25 @@
 class Passenger
-  attr_reader :name, :age
+  attr_reader :name, :age, :driver
 
   def initialize(data)
     @name = data["name"]
     @age = data["age"]
+    @driver = false
   end
 
+  def adult?
+    if @age >= 18
+      @age = true
+    else
+      @age = false
+    end
+  end
 
-
+  def driver?
+    @driver
+  end
+  
+  def drive
+      @driver = true
+  end
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -17,13 +17,31 @@ RSpec.describe Passenger do
     it ' has readable attributes' do
       expect(@charlie.name).to eq("Charlie")
       expect(@charlie.age).to eq(18)
+      
 
       expect(@taylor.name).to eq("Taylor")
       expect(@taylor.age).to eq(12)
+      
     end
-
   end
 
+  describe '#adult' do
+    it 'can check if the driver is an adult' do
+      expect(@charlie.adult?).to eq(true)
+      expect(@taylor.adult?).to eq(false)
+    end
+  end
+
+  describe '#driver' do
+    it 'can check if the passenger is a driver' do
+     
+      expect(@charlie.driver?).to eq(false)
+      
+      @charlie.drive
+
+      expect(@charlie.driver?).to eq(true)
+    end
+  end
 
 
 
